4fa5afbf5f689265bf77234fe233ae2ef6c37868
manjustice <manjustice1998@gmail.com>
2023-07-11 20:57:57 +0300

    Changed get session

--- app/core/database.py
+++ app/core/database.py
@@ -12,5 +12,9 @@ engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
 async_session = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
-
 Base: Any = declarative_base()
+
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
