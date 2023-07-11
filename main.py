e42776e640e4bb44e20b081516508f68a4c1fc76
manjustice <manjustice1998@gmail.com>
2023-07-11 23:22:00 +0300

    changed host to 0.0.0.0

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ app.include_router(image_router)
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
